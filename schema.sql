de6a20028fd4d663d4c48a7c83d8b383487ae62c
Anastasiia <bjko.smsng@gmail.com>
2025-09-17 13:38:05 +0300

    fix: добавлено поле name в таблицу users

--- schema.sql
+++ schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE users (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     email VARCHAR(55) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
     password CHAR(64),
     contact_info TEXT
 );
